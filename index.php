b61ac746de2c415597a2a11df39ec8b39be10c5c
root <root@dafirst>
2015-09-01 19:56:13 +0000

    Layout fix

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 
 <div class="container frame">
 
-  <nav class="col-md-2 col-sm-2" id="navigation-column-one"></nav>
+  <nav class="col-md-1 col-sm-1" id="navigation-column-title"><a href="http://93.95.228.60/projects/ccap/" class="logo">ccap</a>
+</nav>
+  
+  <nav class="col-md-1 col-sm-2" id="navigation-column-one"></nav>
 
-  <nav class="col-md-2 col-sm-2" id="navigation-column-two">
+  <nav class="col-md-1 col-sm-1" id="navigation-column-two">
     <div id="inner-nav-container-column-two" class="mobile-nav"></div>
   </nav>
 
@@ -14,7 +17,7 @@
     <div id="inner-nav-container-column-three" class="mobile-nav"></div>
   </nav>
 
-  <main class="col-md-6 col-sm-6" id="main-content-area">
+  <main class="col-md-7 col-sm-6" id="main-content-area">
     <div id="inner-container-main-content-area" class="mobile-nav"></div>
   </main>
 
